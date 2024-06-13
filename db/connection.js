255eafb8ebca13fcf07ca7a146549819baefb86b
Gioavnna Mierzwa <giihmierzwa111@outlook.com>
2024-06-12 22:10:59 -0300

    Baixando o sql e inicializando a conexão

---
+++ db/connection.js
@@ -0,0 +1,5 @@
+//Baixei o sequelize usando: npm install --save sequelize mysql2
+//Criei uma pasta chamada db e dentro dela um arquivo chamado connection.js onde vou realizar a conecção com o banco de dados MySql
+
+//Chama o Sequelize
+const Sequelize = require('sequelize')
